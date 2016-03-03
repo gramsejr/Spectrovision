63abe6ec92b7fbfed4edd36d2b9181b44960031b
Jason Gramse <jason.gramse@apogeeinstruments.com>
2016-03-03 16:28:53 -0700

    forgot a couple files

---
+++ INI_Configuration.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import logging
+import os
+import platform
+import string
+from tempfile import gettempdir
+
+from ConfigParser import ConfigParser, NoSectionError, NoOptionError
+
+class Config(object):
+    DEFAULT_INI_SECTION = 'Apogee'
+    def __init__(self, ini_defaults, name):
+        super(Config, self).__setattr__('ini_defaults', ini_defaults)
+        super(Config, self).__setattr__('class_name', name)
+        directory = os.path.dirname(self.file_name)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+    @property
+    def file_name(self):
+        system = platform.system()
+        if system == "Darwin":
+            return os.path.join(os.environ['TMPDIR'],
+                                self.DEFAULT_INI_SECTION,
+                                '%s.ini' % self.class_name)
+        elif system == "Windows":
+            return os.path.join(os.environ['APPDATA'],
+                                self.DEFAULT_INI_SECTION,
+                                '%s.ini' % self.class_name)
+
+    def __getattr__(self, k):
+        try:
+            return self.get_config_val(k)
+        except (AttributeError, KeyError):
+            pass
+        if k in self.ini_defaults:
+            self.__setattr__(k, self.ini_defaults[k])
+            return self.ini_defaults[k]
+        raise AttributeError("Doesn't exist in other class nor INI")
+
+    def __setattr__(self, k, v):
+        if not k.lower() == k:
+            raise Exception("Key must be lower case")
+        config = self.get_config()
+        config[k] = v
+        self.set_config(config)
+
+    def get_config(self):
+        cp = ConfigParser()
+        cp.read(self.ini_file_name)
+        config = {}
+        try:
+            options = cp.options(self.DEFAULT_INI_SECTION)
+        except NoSectionError:
+            return config
+        for opt in options:
+            config_val = cp.get(self.DEFAULT_INI_SECTION, opt).strip()
+            config[opt.lower()] = config_val
+        return config
+
+    def get_config_val(self, key):
+        config = self.get_config()
+        return config[key]
+
+    def set_config(self, config):
+        cp = ConfigParser()
+        cp.add_section(self.DEFAULT_INI_SECTION)
+        for k, v in config.items():
+            cp.set(self.DEFAULT_INI_SECTION, k, v)
+        with open(self.ini_file_name, "w") as f:
+            cp.write(f)
+
+
+class INIMixin(object):
+    def __init__(self, *args, **kwargs):
+        ini_defaults = {}
+        if 'ini_defaults' in kwargs:
+            ini_defaults = kwargs.pop('ini_defaults')
+        self.ini = Config(ini_defaults, self.__class__.__name__)
+        super(INIMixin, self).__init__(*args, **kwargs)
---
+++ Messages.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import wx
+import wx.lib.masked as masked
+
+
+def ok_cancel(window, title, msg):
+    dlg = wx.MessageDialog(window, msg, title,
+                           wx.OK | wx.CANCEL | wx.ICON_INFORMATION)
+    response = dlg.ShowModal() == wx.ID_OK
+    dlg.Destroy()
+    return response
+
+
+def give_error(window, title, msg):
+    dlg = wx.MessageDialog(window, msg, title,
+                           wx.OK | wx.STAY_ON_TOP | wx.ICON_HAND)
+    dlg.ShowModal()
+    dlg.Destroy()
+
+
+def confirmation_message(msg, title):
+    dlg = wx.MessageBox(msg, title, wx.OK | wx.ICON_INFORMATION)
+
+
+def progress_dialog(title, msg, window, maximum):
+    if maximum:
+        dlg = wx.GenericProgressDialog(title, msg, maximum=maximum, parent=window,
+                                style=wx.PD_CAN_ABORT | wx.PD_APP_MODAL | wx.PD_AUTO_HIDE)
+    else:
+        dlg = wx.GenericProgressDialog(title, msg, parent=window,
+                                style=wx.PD_CAN_ABORT | wx.PD_APP_MODAL | wx.PD_AUTO_HIDE)
+    dlg.CenterOnScreen()
+    return dlg
+
+def time_control(window, title, text):
+    dlg = wx.Dialog(window, -1, title)
+    text = wx.StaticText(dlg, -1, text)
+    spin = wx.SpinButton(dlg, -1, wx.DefaultPosition, (-1,30), wx.SP_VERTICAL)
+    time24 = masked.TimeCtrl(dlg, -1, fmt24hr=True, spinButton=spin)
+    ok = wx.Button(dlg, wx.ID_OK)
+    cancel = wx.Button(dlg, wx.ID_CANCEL)
+    btn_sizer = wx.StdDialogButtonSizer()
+    h_sizer = wx.BoxSizer(wx.HORIZONTAL)
+    sizer = wx.BoxSizer(wx.VERTICAL)
+    sizer.Add(text, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+    h_sizer.Add(time24, 0, wx.ALIGN_CENTER | wx.ALL, 0)
+    h_sizer.Add(spin, 0, wx.ALIGN_CENTER | wx.ALL, 0)
+    sizer.Add(h_sizer, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+    btn_sizer.Add(ok, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+    btn_sizer.Add(cancel, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+    sizer.Add(btn_sizer, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+    dlg.SetSizer(sizer)
+    dlg.Fit()
+    dlg.CenterOnScreen()
+    start_time = None
+    if dlg.ShowModal() == wx.ID_OK:
+        start_time = time24.GetValue()
+    dlg.Destroy()
+    return start_time
+
+def open_file_dialog(window, title, wildcard="", current_dir="", current_file=""):
+    dlg = wx.FileDialog(window, title, current_dir, current_file, wildcard,
+                        style=wx.FD_OPEN)
+    while dlg.ShowModal() == wx.ID_OK:
+        path = dlg.GetPath()
+        dlg.Destroy()
+        return path
+    dlg.Destroy()
+
+def save_file_dialog(window, title, wildcard="", current_dir="", current_file="", overwrite_prompt=True):
+    if overwrite_prompt:
+        dlg = wx.FileDialog(window, title, current_dir, current_file, wildcard,
+                            style=wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT)
+    else:
+        dlg = wx.FileDialog(window, title, current_dir, current_file, wildcard,
+                            style=wx.FD_SAVE)
+    while dlg.ShowModal() == wx.ID_OK:
+        path = dlg.GetPath()
+        dlg.Destroy()
+        return path
+    dlg.Destroy()
